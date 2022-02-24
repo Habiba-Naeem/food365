e48ae586024715e36e65cdaf59d75ba381ae5ca2
Mahnoor Jamal <greatmahnoor@yahoo.com>
2022-02-24 11:26:33 +0500

    old

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:food365/presentation/modules/home/home_screen.dart';
+import 'package:food365/presentation/modules/onboarding_screen.dart';
+
+void main() {
+  runApp(splash_screen());
+}
 
 class splash_screen extends StatelessWidget {
   @override
@@ -10,7 +14,7 @@ class splash_screen extends StatelessWidget {
         home: AnimatedSplashScreen(
             duration: 4000,
             splash: 'assets/images/menu_items/Food365.png',
-            nextScreen: HomeScreen(),
+            nextScreen: OnboardingScreen(),
             splashTransition: SplashTransition.slideTransition,
             //pageTransitionType: PageTransitionType.scale,
             backgroundColor: Color.fromARGB(255, 230, 178, 214)));
