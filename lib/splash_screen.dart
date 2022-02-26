7859e030126a5f6b8727143d81cd7a61fa3b8d30
Mahnoor Jamal <greatmahnoor@yahoo.com>
2022-02-26 11:40:36 +0500

    splashscreen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,22 +1,27 @@
+
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:food365/presentation/modules/onboarding_screen.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-void main() {
-  runApp(splash_screen());
-}
 
-class splash_screen extends StatelessWidget {
+
+class SplashScreenPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Food365',
-        home: AnimatedSplashScreen(
-            duration: 4000,
-            splash: 'assets/images/menu_items/Food365.png',
-            nextScreen: OnboardingScreen(),
-            splashTransition: SplashTransition.slideTransition,
-            //pageTransitionType: PageTransitionType.scale,
-            backgroundColor: Color.fromARGB(255, 230, 178, 214)));
+    return SplashScreen(
+      seconds: 5,
+      navigateAfterSeconds: new OnboardingScreen(),
+      
+      backgroundColor: Colors.white,
+      title: new Text(
+        'Food365',
+        textScaleFactor: 5,
+      ),
+      image: new Image.network(''),
+      loadingText: Text("Loading"),
+      photoSize: 100.0,
+      loaderColor: Colors.black,
+    );
   }
 }
