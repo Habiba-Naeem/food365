d1a52baa7fe21d25fe7815a57ec52984304276db
Habiba-Naeem <habiba.abdulnaeem@gmail.com>
2022-07-01 22:17:51 +0500

    timer provider changes

--- lib/domain/models/providers/timer_provider.dart
+++ lib/domain/models/providers/timer_provider.dart
@@ -1,21 +1,31 @@
-// import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:food365/domain/models/modules/ordering/menu_item_model.dart';
+import 'package:food365/domain/models/modules/ordering/order_item.dart';
+import 'package:food365/domain/services/menu_service.dart';
 
-// class TimerProvider extends ChangeNotifier {
-//   Duration? avgTime;
-//   final String orderID;
-//   //final List<Duration> itemTimes;
+class TimerProvider extends ChangeNotifier {
+  //final String orderID;
+  //final List<Duration> itemTimes;
 
-//   TimerProvider({
-//     required this.orderID,
-//     //required this.itemTimes,
-//   });
+  Future<Duration> getTime({
+    required order,
+  }) async {
+    List<OrderItem> allItems = order.allOrderItems;
+    Duration totalTime = Duration();
+    List<Future<Duration>> times = allItems.map((item) async {
+      MenuItemModel menuItem =
+           MenuService().getMenuItem(menuItemID: item.menuItemID);
+      print(menuItem.time);
+      //totalTime =  totalTime + menuItem.time;
+      return menuItem.time;
+    }).toList();
+     
+     //totalTime = await times.fold(times.first, (previousValue, element) => previousValue);
+     print(totalTime);
+    return totalTime;
+  }
 
-//   getTimeLeft({required List<Duration> itemTimes}) {
-//     avgTime = itemTimes.forEach((time) {
-//        avgTime + time;
-//     });
-//   }
-//   // factory TimerProvider.fromJson() {
-//   //   return TimerProvider(orderID: orderID, itemTimes: itemTimes);
-//   // }
-// }
+  // factory TimerProvider.fromJson() {
+  //   return TimerProvider(orderID: orderID, itemTimes: itemTimes);
+  // }
+}
